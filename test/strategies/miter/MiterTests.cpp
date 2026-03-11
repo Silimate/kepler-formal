6a8035c3f3bcaf344f1ed9570fa5723bf30cf4e7
Noam Cohen <noam.chn1@gmail.com>
2026-03-11 15:04:57 +0100

    cov

--- test/strategies/miter/MiterTests.cpp
+++ test/strategies/miter/MiterTests.cpp
@@ -1258,6 +1258,30 @@ TEST(KeplerCliSubprocessTests, ExampleTestRunFailure) {
   EXPECT_NE(rc, EXIT_SUCCESS);
 }
 
+// Create 2 citcuits with 2 top inouts ports connected to each others ane expect a passing test
+TEST(KeplerCliSubprocessTests, ConnectedInouts) {
+  NLUniverse* univ = NLUniverse::create();
+  NLDB* db = NLDB::create(univ);
+  NLLibrary* libraryS =
+      NLLibrary::create(db, NLLibrary::Type::Standard, NLName("Stadarts"));
+  // create a top model with 2 inout ports
+  SNLDesign* top =
+      SNLDesign::create(libraryS, SNLDesign::Type::Standard, NLName("top"));
+  univ->setTopDesign(top);
+  auto topInout1 =
+      SNLScalarTerm::create(top, SNLTerm::Direction::InOut, NLName("inout1"));
+  auto topInout2 =
+      SNLScalarTerm::create(top, SNLTerm::Direction::InOut, NLName("inout2"));
+  // connect the 2 inouts together
+  SNLNet* net = SNLScalarNet::create(top, NLName("net"));
+  topInout1->setNet(net);
+  topInout2->setNet(net);
+  SNLDesign* topClone = top->clone(NLName("topClone"));
+  MiterStrategy MiterS(top, topClone, "ConnectedInouts");
+  MiterS.init();
+  EXPECT_TRUE(MiterS.run());
+}
+
 
 // Required main function for Google Test
 int main(int argc, char** argv) {
